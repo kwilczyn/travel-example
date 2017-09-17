909f9aa11a34a66c5b52633faa51ed6e367e42b8
kwilczyn <krzysztof.wilczynski@live.com>
2017-09-17 19:47:04 +0200

    Section 5 /12

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var watch = require('gulp-watch');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
 	console.log("Horray - you created a Gulp task.");
@@ -15,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-	 .pipe(postcss([cssvars, nested, autoprefixer]))
+	 .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 	 .pipe(gulp.dest('./app/temp/styles'));
 });
 
